50dbc0014a3d17f40868f5d2ba32252454efdbe4
EC2 Default User <ec2-user@ip-172-31-41-89.ap-northeast-1.compute.internal>
2022-09-08 12:25:59 +0000

    [Finish]3-12 課題２完成

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
 
   post'books' => 'books#create'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show'
-  get 'books/edit'
-  get '/top' => 'homes#top'
+  get 'books/:id' => 'books#show',as:'book'
+  get 'books/:id/edit' => 'books#edit',as:'edit_book'
+  patch 'books/:id' => 'books#update',as:'update_book'
+  delete'books/:id' => 'books#destroy',as:'destroy_book'
+  root :to => 'homes#top'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
